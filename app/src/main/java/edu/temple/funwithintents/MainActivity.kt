423fdb3478f04c86c5e2c3c38d2af6f97fa9afb7
hsinjungLee <144487630+hsinjungLee@users.noreply.github.com>
2023-11-07 14:42:13 -0500

    add Intent function to share the text when user click the button.

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,21 +1,33 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
 
+        val TEXT = editText.text.toString()
+
         // When the user clicks this button, share the text
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, TEXT)
+                type = "text/plain"
+            }
 
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
         }
     }
 }
